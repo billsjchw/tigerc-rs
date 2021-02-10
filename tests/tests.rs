cfae14b2863c03d7ade436642996ba02a9ced6c1
billsjchw <billsjc@126.com>
2021-02-11 01:06:13 +0800

    test: add integration test testcase merge

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,46 +1,58 @@
 use std::{fs, process::Command};
 
+use fs::File;
+
 #[test]
 fn test_queens() {
-    test("queens");
+    test("queens", false);
 }
 
 #[test]
 fn test_dec2bin() {
-    test("dec2bin");
+    test("dec2bin", false);
 }
 
-fn test(name: &str) {
+#[test]
+fn test_merge() {
+    test("merge", true);
+}
+
+fn test(name: &str, has_input: bool) {
     let prog = format!("tests/testcases/{}.tig", name);
     let obj = format!("tests/{}.o", name);
     let exec = format!("tests/{}", name);
-    let out = format!("tests/testcases/{}.out", name);
+    let output = format!("tests/testcases/{}.out", name);
 
-    Command::new("cargo").args(&[
-        "run",
-        "--",
-        &prog[..],
-        "-o",
-        &obj[..],
-    ]).output().unwrap();
-    Command::new("cc").args(&[
-        "src/tiger.c",
-        &obj[..],
-        "-o",
-        &exec[..],
-    ]).output().unwrap();
+    Command::new("cargo")
+        .args(&["run", "--", &prog[..], "-o", &obj[..]])
+        .output()
+        .unwrap();
+    Command::new("cc")
+        .args(&["src/tiger.c", &obj[..], "-o", &exec[..]])
+        .output()
+        .unwrap();
 
-    let actual = Command::new(&exec[..]).output().unwrap().stdout;
+    let actual = if has_input {
+        let input = format!("tests/testcases/{}.in", name);
+        let input_file = File::open(&input[..]).unwrap();
+        Command::new(&exec[..])
+            .stdin(input_file)
+            .output()
+            .unwrap()
+            .stdout
+    } else {
+        Command::new(&exec[..]).output().unwrap().stdout
+    };
 
-    Command::new("rm").args(&[
-        "-rf",
-        &obj[..],
-    ]).output().unwrap();
-    Command::new("rm").args(&[
-        "-rf",
-        &exec[..],
-    ]).output().unwrap();
+    Command::new("rm")
+        .args(&["-rf", &obj[..]])
+        .output()
+        .unwrap();
+    Command::new("rm")
+        .args(&["-rf", &exec[..]])
+        .output()
+        .unwrap();
 
-    let expected = fs::read(&out[..]).unwrap();
+    let expected = fs::read(&output[..]).unwrap();
     assert_eq!(actual, expected);
 }
